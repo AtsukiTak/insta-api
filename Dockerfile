8d1af153889516247f6f46a1f9c2960c87aa4900
AtsukiTak <takatomgoo@gmail.com>
2018-06-17 02:45:30 +0900

    use instalooter

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,11 @@
 From alpine:edge
 
-RUN apk add --update \
-      udev \
-      ttf-freefont \
-      xvfb \
-      chromium \
-      chromium-chromedriver \
-      python3 \
-      py-pip
-
-RUN pip3 install --upgrade pip && \
-    pip3 install selenium beautifulsoup4 bottle
+RUN apk add --update python3 py-pip && \
+    pip3 install --upgrade pip && \
+    pip3 install bottle paste instalooter
 
 WORKDIR /home/scraper
 
 COPY . /home/scraper
 
-ENTRYPOINT ["python", "server.py"]
+ENTRYPOINT ["python3", "server.py"]
--- server.py
+++ server.py
@@ -1,33 +1,41 @@
 # coding: UTF-8
-from http.server import HTTPServer, BaseHTTPRequestHandler
-from socketserver import ThreadingMixIn
-from bottle import get, run, request, abort
+from bottle import get, Bottle
+from paste import httpserver
+from instalooter.looters import HashtagLooter, PostLooter
+
+from itertools import islice
 import os
 
-from tag_scraper import TagScraper
-from post_scraper import PostScraper
-from web_driver import init_driver
+app = Bottle()
 
-driver = init_driver()
+@app.get('/hashtags/<hashtag>')
+def get_hashtag(hashtag):
+    print("get hashtag : %s" % hashtag)
+    looter = HashtagLooter(hashtag)
+    medias = islice(looter.medias(), 100)
+    res = list(map(media_to_dict, medias))
+    return { "res": res }
 
-@get('/posts/<post_id>')
-def get_post_by_id(post_id):
-    post = PostScraper(driver).scrape(post_id)
-    post['post_id'] = post_id
+def media_to_dict(media):
+    dic = {
+        "id": media["shortcode"],
+        "image_url": media["display_url"],
+        "user_id": media["owner"]["id"],
+    }
+    return dic
+
+
+@app.get('/posts/<post_id>')
+def get_post(post_id):
+    print("get post : %s" % post_id)
+    looter = PostLooter(post_id)
+    post = looter.get_post_info(post_id)
     return post
-
-@get('/posts')
-def get_posts():
-    hashtag = request.query.get('hashtag')
-    if hashtag is None:
-        return abort(400, "you must set hashtag parameter")
-    posts = TagScraper(driver).scrape(hashtag)
-    return {'posts': posts}
 
 def run_server():
     host = os.getenv("HOST", "0.0.0.0")
     port = os.getenv("PORT", "8000")
     debug = os.getenv("DEBUG", True)
-    run(host = host, port = port, debug = debug)
+    httpserver.serve(app, host = host, port = port)
 
 run_server()
